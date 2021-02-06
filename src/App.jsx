ef0cd88ffcab346605c7a97ada710a38da535344
RolanGetz <gerarovf@gmail.com>
2021-02-06 22:05:57 +0300

    grid css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import './App.css';
 
 function App() {
     const [boards, setBoards] = useState([
-        {id: 1, title: "Сделать", items: [{id: 1, title: "Пойти в магазин"}, {id: 2, title: "Написать песню"}, {id: 3, title: "Пойти в баню"}, {id: 4, title: "Пойти к тёлкам"}, {id: 5, title: "Не пойти в магазин"}, {id: 6, title: "Работать"}]},
+        {
+            id: 1,
+            title: "Сделать",
+            items: [{id: 1, title: "Выучить Graphql"}, {id: 2, title: "Написать песню"}, {
+                id: 3,
+                title: "Пойти в баню"
+            }, {id: 4, title: "Улететь в космос"}, {id: 5, title: "Заняться спортом"}, {id: 6, title: "Побриться"}]
+        },
         {id: 2, title: "Проверить", items: []},
         {id: 3, title: "Сделано", items: []}
     ])
@@ -65,30 +72,36 @@ function App() {
     }
 
     return (
-        <div className="app">
-            {boards.map(board =>
-                <div
-                    className='board'
-                    onDragOver={(e)=>dragOverHandler(e)}
-                    onDrop={(e) => dropCardHandler(e, board)}
-                >
-                    <div className="board__title">{board.title}</div>
-                    {board.items.map(item =>
+        <>
+            {/*<div className="wrap">*/}
+
+                <div className="app">
+                    <div className="title"><h1>Trello)</h1></div>
+                    {boards.map(board =>
                         <div
+                            className='board'
                             onDragOver={(e) => dragOverHandler(e)}
-                            onDragLeave={e => dragLeaveHandler(e)}
-                            onDragStart={(e) => dragStartHandler(e, board, item)}
-                            onDragEnd={(e) => dragEndHandler(e)}
-                            onDrop={(e) => dropHandler(e, board, item)}
-                            className="todo"
-                            draggable={true}
-                            className="item">
-                            {item.title}
+                            onDrop={(e) => dropCardHandler(e, board)}
+                        >
+                            <div className="board__title">{board.title}</div>
+                            {board.items.map(item =>
+                                <div
+                                    onDragOver={(e) => dragOverHandler(e)}
+                                    onDragLeave={e => dragLeaveHandler(e)}
+                                    onDragStart={(e) => dragStartHandler(e, board, item)}
+                                    onDragEnd={(e) => dragEndHandler(e)}
+                                    onDrop={(e) => dropHandler(e, board, item)}
+                                    className="todo"
+                                    draggable={true}
+                                    className="item">
+                                    {item.title}
+                                </div>
+                            )}
                         </div>
                     )}
                 </div>
-            )}
-        </div>
+            {/*</div>*/}
+        </>
     );
 }
 
